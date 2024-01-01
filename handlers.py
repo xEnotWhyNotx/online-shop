69e13ef9f9cef67d83c5c98a7b8f3e2a5ac879a0
Sergey Novichkov <NovichkovSD@yandex.ru>
2024-01-01 19:57:24 +0300

    Initial commit

---
+++ handlers.py
@@ -0,0 +1,17 @@
+from aiogram import F, Router, types
+from aiogram.filters import Command
+from aiogram.types import Message
+import kb
+import text
+
+router = Router()
+
+@router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu)
+
+@router.message(F.text == "Меню")
+@router.message(F.text == "Выйти в меню")
+@router.message(F.text == "◀️ Выйти в меню")
+async def menu(msg: Message):
+    await msg.answer(text.menu, reply_markup=kb.menu)

751db4f63f7c35689a0fa9006ce0b9ec5b984124
Sergey Novichkov <NovichkovSD@yandex.ru>
2024-04-24 20:53:52 +0300

    test

--- kb.py
+++ kb.py
@@ -10,7 +10,7 @@ menu = [
      InlineKeyboardButton(text="🎁 Бесплатные токены", callback_data="free_tokens")],
     [InlineKeyboardButton(text="🔎 Помощь", callback_data="help")]
 ]
-peremennaya = []
+
 menu = InlineKeyboardMarkup(inline_keyboard=menu)
 exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
 iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
